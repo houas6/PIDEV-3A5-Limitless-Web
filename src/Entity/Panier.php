040eca87b3e6ac098f9854695a71015cc6b9f2ef
alarassaa <80418086+alarassaa@users.noreply.github.com>
2023-03-26 19:03:45 +0200

    panier1.1
    panier1.1

---
+++ src/Entity/Panier.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Panier
+ *
+ * @ORM\Table(name="panier", indexes={@ORM\Index(name="id_user", columns={"id_user"}), @ORM\Index(name="id_produit", columns={"id_produit"})})
+ * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\PanierRepository")
+ */
+class Panier
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_panier", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idPanier;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantite_product", type="integer", nullable=false)
+     */
+    private $quantiteProduct;
+
+    /**
+     * @var \App\Entity\Utilisateur
+     *
+     * @ORM\ManyToOne(targetEntity="Utilisateur")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_user", referencedColumnName="id_user")
+     * })
+     */
+    private $idUser;
+
+    /**
+     * @var \App\Entity\Produit
+     *
+     * @ORM\ManyToOne(targetEntity="Produit")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_produit", referencedColumnName="id_produit")
+     * })
+     */
+    private $idProduit;
+
+    public function getIdPanier(): ?int
+    {
+        return $this->idPanier;
+    }
+
+    public function getQuantiteProduct(): ?int
+    {
+        return $this->quantiteProduct;
+    }
+
+    public function setQuantiteProduct(int $quantiteProduct): self
+    {
+        $this->quantiteProduct = $quantiteProduct;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?Utilisateur
+    {
+        return $this->idUser;
+    }
+
+    public function setIdUser(?Utilisateur $idUser): self
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    public function getIdProduit(): ?Produit
+    {
+        return $this->idProduit;
+    }
+
+    public function setIdProduit(?Produit $idProduit): self
+    {
+        $this->idProduit = $idProduit;
+
+        return $this;
+    }
+
+
+}
