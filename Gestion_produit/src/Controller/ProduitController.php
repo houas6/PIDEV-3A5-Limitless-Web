b3b251cf563de4ef83f0cdf49933a27cb8c788c6
houas Med aziz <medaziz.houas@esprit.tn>
2023-03-28 03:38:54 +0200

    1st push
    crud done without template

---
+++ Gestion_produit/src/Controller/ProduitController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use App\Repository\ProduitRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/produit')]
+class ProduitController extends AbstractController
+{
+    #[Route('/', name: 'app_produit_index', methods: ['GET'])]
+    public function index(ProduitRepository $produitRepository): Response
+    {
+        return $this->render('produit/index.html.twig', [
+            'produits' => $produitRepository->findAll(),
+        ]);
+    }
+
+    /**
+ * @Route("/produit/new", name="produit_new", methods={"GET","POST"})
+ */
+public function new(Request $request): Response
+{
+    $produit = new Produit();
+    $form = $this->createForm(ProduitType::class, $produit);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $data = $form->getData();
+
+        // Set the values of the Produit object
+        $produit->setNomProduit($data->getNomProduit());
+        $produit->setPrix($data->getPrix());
+        $produit->setDescription($data->getDescription());
+        $produit->setIdUser($data->getId_Produit());
+        $produit->setImage($data->getImage());
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($produit);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('produit_index');
+    }
+
+    return $this->render('produit/new.html.twig', [
+        'produit' => $produit,
+        'form' => $form->createView(),
+    ]);
+}
+
+
+public function show(Request $request, Produit $produit): Response
+{
+    return $this->render('produit/show.html.twig', [
+        'produit' => $produit,
+    ]);
+}
+
+
+
+#[Route('/{id_produit}/edit', name: 'app_produit_edit', methods: ['GET', 'POST'])]
+public function edit(Request $request, Produit $produit): Response
+{
+    $form = $this->createForm(ProduitType::class, $produit, [
+        'action' => $this->generateUrl('app_produit_edit', ['id_produit' => $produit->getId_Produit()])
+    ]);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    return $this->renderForm('produit/edit.html.twig', [
+        'produit' => $produit,
+        'form' => $form,
+    ]);
+}
+
+#[Route('/{id_produit}', name: 'app_produit_delete', methods: ['POST'])]
+public function delete(Request $request, Produit $produit): Response
+{
+    if ($this->isCsrfTokenValid('delete'.$produit->getId_Produit(), $request->request->get('_token'))) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($produit);
+        $entityManager->flush();
+    }
+
+    return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+}
+
+}
---
+++ Gestion_produit/src/Entity/Produit.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Produit
+ *
+ * @ORM\Table(name="produit", indexes={@ORM\Index(name="FK_Pu", columns={"id_user"})})
+ * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_produit", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id_produit;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_produit", type="string", length=30, nullable=false)
+     */
+    private $nom_produit;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prix", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $prix;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=30, nullable=false)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=30, nullable=false)
+     */
+    private $image;
+
+    /**
+     * @var \Utilisateur
+     *
+     * @ORM\ManyToOne(targetEntity="App\Entity\Utilisateur")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_user", referencedColumnName="id_user")
+     * })
+     */
+    private $idUser;
+
+    
+
+    
+
+    public function getId_produit(): ?int
+    {
+        return $this->id_produit;
+    }
+
+    public function setId_produit(int $id_produit): self
+    {
+        $this->id_produit = $id_produit;
+
+        return $this;
+    }
+
+    public function getNomProduit(): ?string
+    {
+        return $this->nom_produit;
+    }
+
+    public function setNomProduit(string $nom_produit): self
+    {
+        $this->nom_produit = $nom_produit;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?Utilisateur
+    {
+        return $this->idUser;
+    }
+
+    public function setIdUser(?Utilisateur $utilisateur): self
+    {
+        $this->idUser = $utilisateur;
+
+        return $this;
+    }
+
+
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
