03939f967fad1d1b65a6a98a160e46786ef8ab75
dhiasaibi <dhiasaibi@yahoo.com>
2023-04-03 14:49:38 +0100

    Template

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-Gestion_User',
+        'version' => 'dev-Gestion_User',
+        'reference' => '5f8396a9f57bfd9a02cc2bea15f3662298c06927',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-Gestion_User',
+            'version' => 'dev-Gestion_User',
+            'reference' => '5f8396a9f57bfd9a02cc2bea15f3662298c06927',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
