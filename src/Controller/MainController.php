9335de200a26f58d3509f220530b302e99181bcb
dhiasaibi <dhiasaibi@yahoo.com>
2023-04-07 11:05:08 +0100

    login and sigunp templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,10 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/base', name: 'app_base')]
     public function index(): Response
     {
+        return $this->render('base.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/main', name: 'app_main')]
+    public function indexMain(): Response
+    {
         return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function indexLogin(): Response
+    {
+        return $this->render('main/login.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function indexRegister(): Response
+    {
+        return $this->render('main/register.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit3cbcb61d5f1b562475588aa94fb02e1f
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit3cbcb61d5f1b562475588aa94fb02e1f', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit3cbcb61d5f1b562475588aa94fb02e1f', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit3cbcb61d5f1b562475588aa94fb02e1f
         call_user_func(\Composer\Autoload\ComposerStaticInit3cbcb61d5f1b562475588aa94fb02e1f::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit3cbcb61d5f1b562475588aa94fb02e1f::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
